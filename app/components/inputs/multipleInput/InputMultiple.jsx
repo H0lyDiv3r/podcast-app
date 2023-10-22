b37f0dd0c9fb43b034d4065ced18f1f47c34e13c
H0lyDiv3r <67262860+H0lyDiv3r@users.noreply.github.com>
2023-10-22 18:17:04 +0300

    finished multiple select component

--- app/components/inputs/multipleInput/InputMultiple.jsx
+++ app/components/inputs/multipleInput/InputMultiple.jsx
@@ -1,19 +1,30 @@
+
 import { Select, Box, Text, Icon } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { topGenres,gens } from '@/app/data/genres'
 
 const InputMultiple = () => {
-    const [genres,setGenres] = useState(["Entertainment","News"])
+    const [genres,setGenres] = useState([])
+
+    const handleAddGenre = (value)=>{
+        if(!genres.includes(value)){
+            setGenres([...genres,value])
+        }
+    }
   return (
     <Box>
         <Box display={"flex"}>
+            <Box display={"flex"} flexWrap={"wrap"}>   
             {genres.map(genre=>
-                <GenreWrapper>{genre}</GenreWrapper>
+                <GenreWrapper key={genre}>{gens[genre]}</GenreWrapper>
             )}
+            </Box>
         </Box>
-        <Select onChange={()=>setGenres(["lalala"])}>
-            <option value={"crime"}>Crime</option>
-            <option value={"true"}>True</option>
+        <Select onChange={(e)=>handleAddGenre(e.target.value)}>
+                {Object.keys(gens).map(gen=>
+                    <option key={gen} value={gen} >{gens[gen]}</option>
+                )}
         </Select>
     </Box>
   )
@@ -24,11 +35,11 @@ export default InputMultiple
 export const GenreWrapper = ({children})=>{
 
     return(
-        <>
-            <Icon as={FaTimes} bg={"rose.600"} padding={"2px"} borderRadius={"50%"} boxSize={4} color={"white"} position={"relative"} top={"-6px"} left={"10px"}/>
-            <Box bg={"rose.500"} color={'white'} marginRight={"10px"} py={"2px"} px={"8px"} borderRadius={"5px"}>
+        <Box display={"flex"}>
+            <Icon as={FaTimes} bg={"rose.600"} padding={"2px"} borderRadius={"50%"} boxSize={4} color={"white"} position={"relative"} top={"5px"} left={"10px"}/>
+            <Box bg={"rose.500"} color={'white'} marginRight={"10px"} py={"2px"} px={"8px"} borderRadius={"5px"} fontSize={"14px"} my={"10px"} >
                 {children}
             </Box>
-        </>
+        </Box>
     )
 }
--- app/data/genres.js
+++ app/data/genres.js
@@ -800,6 +800,137 @@ const genres =[
       "parent_id": 99
   }
 ]
+
+export const topGenres = [
+      {
+        "id": 144,
+        "name": "Personal Finance",
+        "parent_id": 67
+      },
+      {
+        "id": 151,
+        "name": "Locally Focused",
+        "parent_id": 67
+      },
+      {
+        "id": 93,
+        "name": "Business",
+        "parent_id": 67
+      },
+      {
+        "id": 77,
+        "name": "Sports",
+        "parent_id": 67
+      },
+      {
+        "id": 125,
+        "name": "History",
+        "parent_id": 67
+      },
+      {
+        "id": 122,
+        "name": "Society & Culture",
+        "parent_id": 67
+      },
+      {
+        "id": 127,
+        "name": "Technology",
+        "parent_id": 67
+      },
+      {
+        "id": 132,
+        "name": "Kids & Family",
+        "parent_id": 67
+      },
+      {
+        "id": 168,
+        "name": "Fiction",
+        "parent_id": 67
+      },
+      {
+        "id": 88,
+        "name": "Health & Fitness",
+        "parent_id": 67
+      },
+      {
+        "id": 134,
+        "name": "Music",
+        "parent_id": 67
+      },
+      {
+        "id": 99,
+        "name": "News",
+        "parent_id": 67
+      },
+      {
+        "id": 133,
+        "name": "Comedy",
+        "parent_id": 67
+      },
+      {
+        "id": 100,
+        "name": "Arts",
+        "parent_id": 67
+      },
+      {
+        "id": 69,
+        "name": "Religion & Spirituality",
+        "parent_id": 67
+      },
+      {
+        "id": 117,
+        "name": "Government",
+        "parent_id": 67
+      },
+      {
+        "id": 68,
+        "name": "TV & Film",
+        "parent_id": 67
+      },
+      {
+        "id": 82,
+        "name": "Leisure",
+        "parent_id": 67
+      },
+      {
+        "id": 111,
+        "name": "Education",
+        "parent_id": 67
+      },
+      {
+        "id": 107,
+        "name": "Science",
+        "parent_id": 67
+      },
+      {
+        "id": 135,
+        "name": "True Crime",
+        "parent_id": 67
+      }
+    ]
+   export const gens =  {
+        144:"Personal Finance",
+        151:"Locally Focused",
+        93:"Business",
+        77:"Sports",
+        125:"History",
+        122:"Society & Culture",
+        127:"Technology",
+        132:"Kids & Family",
+        168:"Fiction",
+        88:"Health & Fitness",
+        134:"Music",
+        99:"News",
+        133:"Comedy",
+        100:"Arts",
+        69:"Religion & Spirituality",
+        117:"Government",
+        68:"TV & Film",
+        82:"Leisure",
+        111:"Education",
+        107:"Science",
+        135: "True Crime"
+    }
   
 
   const getGenre = (id)=>{
